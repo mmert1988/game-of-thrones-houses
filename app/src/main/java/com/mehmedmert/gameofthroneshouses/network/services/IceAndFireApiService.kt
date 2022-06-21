7db52dd47733224ce0a21fcecf708eb28d6be837
Mehmed Mert <mehmed.mert@elasticbrains.de>
2022-06-21 17:02:13 +0200

    Fixing getHouse call

--- app/src/main/java/com/mehmedmert/gameofthroneshouses/network/services/IceAndFireApiService.kt
+++ app/src/main/java/com/mehmedmert/gameofthroneshouses/network/services/IceAndFireApiService.kt
@@ -3,11 +3,12 @@ package com.mehmedmert.gameofthroneshouses.network.services
 import com.mehmedmert.gameofthroneshouses.network.model.HouseApiModel
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface IceAndFireApiService {
     @GET("houses")
     suspend fun getHouses() : Response<List<HouseApiModel>>
 
     @GET("houses/{id}")
-    suspend fun getHouse(id: String) : Response<HouseApiModel>
+    suspend fun getHouse(@Path("id") id: String) : Response<HouseApiModel>
 }
